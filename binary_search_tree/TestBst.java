09981f9e55ef740c36de8e9e0cae99a60faed5c5
Rohit Khilnani <rkkhilna@ncsu.edu>
2015-12-26 18:51:04 +0530

    Add test Case for search

--- binary_search_tree/TestBst.java
+++ binary_search_tree/TestBst.java
@@ -2,7 +2,7 @@ package binary_search_tree;
 
 import java.util.NoSuchElementException;
 import binary_search_tree.Bst;	// My implementation of Binary Search Tree in package binary_search_tree
-
+import binary_search_tree.Node;	// My implementation of Node for BST in package binary_search_tree.
 /*
 	Class to test class Bst. (Binary Search Tree)
 */
@@ -96,12 +96,60 @@ class TestBst{
 	}
 
 
+	// Tests Search
 
+	public static void testSearch(){
+
+		// Create a bst object
+		Bst bst = new Bst();
+
+
+		// Test search on empty tree. Should throw exception
+		try{
+
+			bst.search(10);	
+		}
+		catch(NoSuchElementException e){
+
+			System.out.println(e);
+		}
+
+
+		// Create array of integers to insert
+		int[] values = {10, 7, 9, 8, 2, 15, 13, 17};
+		
+
+		// Insert values into bst
+		for(int v: values){
+			bst.insert(v);
+		}
+
+		// Create array of keys to search
+		int[] searchKeys = {10,7,15,2,9,13,17,8,1,19,12,5};
+
+		// Search for each key in bst
+		for(int s: searchKeys){
+
+			Node n = bst.search(s);
+
+			// Print message based on whether search is successful or unsuccessful.
+			if(n!=null)
+				System.out.println(s+" Found! Node contents: "+n.getValue());
+			else
+				System.out.println(s+" Not Found!");
+		}
+
+
+
+	}
 
 	public static void main(String args[]){
 
 		// Test Insertion
 		testInsertion();
+
+		// Test Search
+		testSearch();
 	}
 
 }
