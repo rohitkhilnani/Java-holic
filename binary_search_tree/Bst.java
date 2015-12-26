b1797f05ba3a99144fdf5c4aa982798e280fd6f9
Rohit Khilnani <rkkhilna@ncsu.edu>
2015-12-26 14:51:40 +0530

    Create classes Bst and TestBst. Add methods to insert into bst, do in order traversal and test insert

---
+++ binary_search_tree/Bst.java
@@ -0,0 +1,102 @@
+package binary_search_tree;
+
+import java.util.NoSuchElementException;
+import binary_search_tree.Node;		// My implementation of Node class present in package binary_search_tree
+
+/*
+	This class implements Binary Search Tree. Following algorithms have been implemented: 
+
+						1. Insertion
+						2. Deletion
+						3. In Order Traversal
+						4. Pre Order Traversal
+						5. Post Order Traversal
+						6. Level Order Traversal
+						7. Search for an element
+
+	Note: Duplicate Keys are not allowed.					
+*/
+
+class Bst{
+
+	private Node root = null; 		// Root of the tree
+
+	// Constructs bst with default values
+	Bst(){
+		// Do Nothing.	
+	}
+
+
+	// Inserts a new node with specified value at specifiec node n.
+	// Time Complexity: 
+	//					Average Case: O(log N)
+	//					Worst Case: O(N)		
+	// Returns Root of sub tree at which the new node is inserted
+
+	protected Node insert(Node n, int value){
+
+		// If n is null, allocate a new node with specified value at n
+		if(n == null){	
+
+			n = new Node(value);
+
+		}
+		else if(value < n.getValue()){ 	 // If value is less than the value at n, insert value in left subtree of n 	
+
+			 n.setLeft( insert(n.getLeft(), value) );
+		}
+		else{
+
+			n.setRight( insert(n.getRight(), value) ); //Otherwise, insert in the right subtree of n
+		}
+
+		// Return the root
+		return n; 
+
+	}
+
+
+	// Inserts a new Node with specified value at Root Node. 
+	// Calls overloaded insert(Node n, int value)
+
+	public void insert(int value){
+
+		root = insert(root,value);
+	}	
+
+
+	// Visits a Node in In Order fashion
+
+	protected void inOrderVisit(Node n){
+
+		// Return if n is null
+		if(n == null)
+			return;
+
+		// Otherwise, visit Left subtree
+		inOrderVisit(n.getLeft());
+
+		// Print value at node
+		System.out.print(n.getValue()+" ");
+
+		// Visit Right subtree
+		inOrderVisit(n.getRight());
+	}
+
+
+	// In Order traversal of tree. Calls inOrderVisit on Root
+	// Throws NoSuchElementException if root is null i.e. tree is empty.
+
+	public void inOrder(){
+
+		// If tree is empty, throw NoSuchElementException
+		if(root == null){
+			throw new NoSuchElementException("Tree is empty");	
+		}
+
+		// Otherwise, call inOrderVisit on root
+		inOrderVisit(root);
+
+	}
+
+}
---
+++ binary_search_tree/TestBst.java
@@ -0,0 +1,44 @@
+package binary_search_tree;
+
+import binary_search_tree.Bst;	// My implementation of Binary Search Tree in package binary_search_tree
+
+/*
+	Class to test class Bst. (Binary Search Tree)
+*/
+
+class TestBst{
+
+			
+
+	// Tests Insertion
+	public static void testInsertion(){
+
+		// Create a bst object
+		Bst bst = new Bst();
+
+		// Create array of integers to insert
+		int[] values = {10, 7, 9, 8, 2, 15, 13, 17};
+
+		// Insert values into bst
+		for(int v: values){
+			bst.insert(v);
+		}
+
+		// Print In Order traversal
+		System.out.print("Inorder Traversal:\t ");
+		bst.inOrder();
+
+		System.out.println();
+		System.out.println("Should be:\t\t 2 7 8 9 10 13 15 17");
+
+	}
+
+
+
+	public static void main(String args[]){
+
+		// Test Insertion
+		testInsertion();
+	}
+
+}
