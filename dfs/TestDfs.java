4534b6bf3905b9a5bdf09d81255a17dac5b6a56d
Rohit Khilnani <rkkhilna@ncsu.edu>
2015-12-28 16:04:43 +0530

    Add test case for topolocial sort

--- dfs/TestDfs.java
+++ dfs/TestDfs.java
@@ -93,7 +93,11 @@ class TestDfs{
 		}
 
 		System.out.println("Is graph cyclic? : " + dfs.isCyclic());
+		System.out.print("Topological Sort: ");
 
+		// Print topological sort
+		dfs.topologicalSort();
+		System.out.println();
 	}
 
 
@@ -134,17 +138,63 @@ class TestDfs{
 		}
 
 		System.out.println("Is graph cyclic? : " + dfs.isCyclic());
+		System.out.print("Topological Sort: ");
+
+		// Print topological sort
+		dfs.topologicalSort();
+		System.out.println();
 
 	}
 
 
+	// Tests Topolocial Sort
+
+	public static void testTopologicalSort(){
+
+		System.out.println("Test 4: \n");
+		int N = 6;	// No. of vertices
+		
+		int M = 13;	// No. fo edges
+
+		int[][] edges = 	{	{1,2}, 		// Edges
+								{1,3},
+								{1,4},
+								{1,5},
+								{1,6},
+								{2,3},
+								{2,4},
+								{2,5},
+								{3,6}, 
+								{3,4},
+								{4,5},
+								{4,6},
+								{5,6},
+								
+
+							};
+
+		Dfs dfs = new Dfs(N, M, edges);		// Use default starting vertex 1
+
+		System.out.println("Topological Sort: ");
+		dfs.topologicalSort();
+
+	}
+
 
 	public static void main(String args[]){
 
-		// call testTraversal
+		// Test Traversal
 		testTraversal();
+		
+		// Test whether cyclic or not
 		testCyclicA();
+		
+		// Test whether cyclic or not
 		testCyclicB();
+
+
+		// Test Topological Sort
+		testTopologicalSort();
 
 	}
 
