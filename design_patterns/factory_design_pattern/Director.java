f9b327daf1feb268580b87d48ab6c7195cf29b97
Rohit Khilnani <rkkhilna@ncsu.edu>
2016-02-23 21:40:59 -0500

    Create abstract class person and its concrete classes

---
+++ design_patterns/factory_design_pattern/Director.java
@@ -0,0 +1,20 @@
+package design_patterns.factory_design_pattern;
+import design_patterns.factory_design_pattern.Person;
+
+/*
+	Class Director - Encapsulates data about a Director 
+	Extends abstract class Person
+*/
+
+class Director extends Person{
+
+	// Constructor
+
+	Director(){
+
+		name = "Director";	
+		access = "Full";		
+	}
+
+}	
+
---
+++ design_patterns/factory_design_pattern/Faculty.java
@@ -0,0 +1,20 @@
+package design_patterns.factory_design_pattern;
+import design_patterns.factory_design_pattern.Person;
+
+/*
+	Class Faculty - Encapsulates data about a Faculty Memeber
+	Extends abstract class Person
+*/
+
+class Faculty extends Person{
+	
+	// Constructor
+
+	Faculty(){
+
+		name = "Faculty";	
+		access = "Enhanced";
+	}
+
+}	
+
